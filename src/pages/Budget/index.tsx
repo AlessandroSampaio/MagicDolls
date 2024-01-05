7f7697c9076d965abfaa7e360c305278ced76dab
AlessandroSampaio <alessandrosampaio521@gmail.com>
2024-01-05 15:16:00 -0300

    Ajustes no formulario de orcamento

--- src/pages/Budget/index.tsx
+++ src/pages/Budget/index.tsx
@@ -1,12 +1,27 @@
 import { Input } from '../../components/Input';
 import { Select } from '../../components/Select';
 import { TextArea } from '../../components/TextArea';
+import whatsapp from '../../assets/images/svg/whatsapp.svg';
 
 export function Budget() {
 
   return (
     <div className="m-10">
-      <form action="" className='flex flex-wrap w-full justify-center items-center'>
+      <div>
+        <div>
+          <p className='font-bold text-lg'>Quais as formas de pagamento?</p>
+          <p>O pagamento pode ser realizado de duas maneiras:
+            1. Se optar pelo pagamento integral via Pix, você receberá um desconto de 7% no valor da peça.
+            2. Optando pelo pagamento normal pode ser dividido em duas parcelas: 50% do valor total é pago no momento do agendamento e os 50% restantes, juntamente com o frete, são pagos após a finalização da peça.
+          <br />
+            Aceitamos <b>Boleto bancario / Pix / Cartão (Mercado Pago)</b></p>
+        </div>
+        <div className='my-6'>
+          <p className='font-bold text-lg'>Quais as formas de envio?</p>
+          <p>Após a finalização da peça, procedemos com o envio. Utilizamos os Correios para realizar entregas em todo o Brasil.</p>
+        </div>
+      </div>
+      <form action="" className='flex flex-wrap w-full justify-center items-center mt-6'>
         <Input id="name" placeholder='nome' type="text" parentStyle='w-full'/>
         <div className='flex w-full mobile:flex-col' >
           <Input id="email" placeholder='email' type="email" parentStyle='w-[70%] mobile:w-full' />
@@ -14,16 +29,16 @@ export function Budget() {
         </div>
         <Input id="endereco" placeholder='Endereço' type="text" className='min-w-[300px]'  parentStyle='w-full'/>
         <div className='flex mobile:flex-col w-full'>
-          <Input id="numero" placeholder='numero' type="number" className='min-w-[50px]' parentStyle='w-[20%]'/>
-          <Input id="bairro" placeholder='Bairro' type="text" className='min-w-[200px]' parentStyle='w-1/2' />
-          <Input id="cep" placeholder='cep' type="text" className='min-w-100px' parentStyle='w-[30%]'/>
+          <Input id="numero" placeholder='numero' type="number" className='min-w-[50px]' parentStyle='w-[20%] mobile:w-full'/>
+          <Input id="bairro" placeholder='Bairro' type="text" className='min-w-[200px]' parentStyle='w-1/2 mobile:w-full' />
+          <Input id="cep" placeholder='cep' type="text" className='min-w-100px' parentStyle='w-[30%] mobile:w-full'/>
         </div>
 
         {/* Replace with custom Select/options */}
         <div className='w-full flex'>
           <Select name="estilo" id="estilo" title='estilo'
             placeholder='Estilo de peça'
-            className='min-w-[200px] w-full border'
+            className='min-w-[200px] w-full'
             parentStyle='w-[70%]'
           >
             <option value="1">Chibi</option>
@@ -52,14 +67,20 @@ export function Budget() {
         />
         <button
           className='
-            mobile:w-full w-1/2
+            mobile:w-full
             bg-teal-400
             p-4
             rounded-xl
             text-white-50
+            flex
+            gap-4
+            justify-center
+            items-center
+            mt-6
           '
         >
           Enviar Orçamento
+          <img src={whatsapp} alt="whatsapp" className='size-8' />
         </button>
       </form>
     </div>
